4dd89cd70302e07adf01e8684bc12d416db482a6
Luke Martin <ljmart02@gmail.com>
2021-02-08 16:42:27 -0700

    Header skeleton setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>React Portfolio</h1 >
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+
+            <h1>
+                This is the header
+            </h1>
+            
+            
+            
+            </nav>
+
+
+    )
+}
+export default Header;
