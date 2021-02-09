44c7faf3625ff628e1871044cd71ab0963a3fc20
Luke Martin <ljmart02@gmail.com>
2021-02-08 19:20:28 -0700

    Updated Nav Bar with clickable locations

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
 import Skills from "./pages/Skills";
+import About from "./pages/About";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Wrapper from "./components/Wrapper/Wrapper";
@@ -14,7 +15,7 @@ function App() {
     <div className="App">
       <Header />
       <Wrapper>
-      <Route exact path="/" component={Skills} />
+      <Route exact path="/" component={About} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/skills" component={Skills} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 function Header() {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Link className="navbar-brand" to="/">
+                Luke M
+             </Link>
+             <div>
+        <ul className="navbar-nav">
 
-            <h1>
-                This is the header
-            </h1>
-            
+          <li className="nav-item">
+            <Link
+              to="/Skills"
+              className={window.location.pathname === "/Skills" ? "nav-link active" : "nav-link"}
+            >
+              Skills
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/Portfolio"
+              className={window.location.pathname === "/Portfolio" ? "nav-link active" : "nav-link"}
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact"
+              className={
+                window.location.pathname === "/contact" || window.location.pathname === "/contact"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
 
-            </nav>
+        </nav>
 
 
     )
