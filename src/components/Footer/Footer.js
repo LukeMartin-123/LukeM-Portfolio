0659307626910fe6feacb034b1ddc9b4cbbec2a2
Luke Martin <ljmart02@gmail.com>
2021-02-10 20:12:35 -0700

    Footer and styles update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,34 @@
 import React from "react";
 import "./styles.css";
 
-function Footer() {
-
-    return (
-        <footer className="footer">
-
-            <h1>
-                This is the footer
-            </h1>
-        
-            </footer>
-
-
-    )
+var style = {
+    backgroundColor: "#292b2c",
+    borderTop: "1px solid #E7E7E7",
+    textAlign: "center",
+    padding: "20px",
+    position: "fixed",
+    left: "0",
+    bottom: "0",
+    height: "60px",
+    width: "100%",
 }
+
+var phantom = {
+    display: 'block',
+    padding: '20px',
+    height: '60px',
+    width: '100%',
+  }
+  
+  function Footer({ children }) {
+      return (
+          <div>
+              <div style={phantom} />
+              <div style={style}>
+                  { children }
+              </div>
+          </div>
+      )
+  }
+  
 export default Footer;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,13 +4,16 @@ import Footer from "../components/Footer/Footer";
 import Row from "../components/Row/Row.js";
 import Col from "../components/Col/Col.js";
 import Container from "../components/Container/Container.js";
+import "./contact.css"
 
 function Contact() {
     return (
       <Router>
-        <div>
-          <Container style={{ marginTop: 30 }}>
+        <div className= "container">
+          <Container style={{ marginTop: 30, }}>
 
+        <h1>Email: Ljmart02@gmail.com</h1>
+        <h1>Phone Number: 610-223-7880</h1>
           </Container>
           <Footer />
         </div>
